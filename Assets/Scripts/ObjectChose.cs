d6223d171de71cc09e77311a99528857675cb620
RobinUth <robin.uth95@gmail.com>
2018-05-28 19:30:14 +0200

    Initial commit

---
+++ Assets/Scripts/ObjectChose.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectChose : Cube {
+
+
+    public GameObject cube;
+    public GameObject sphere;
+    public GameObject capsule;
+
+
+    private void Start()
+    {
+      
+       CubeSpawn();
+
+    
+    }
+    public void NewObject()
+    {
+
+        size = 1;
+        speed = 10;
+    }
+
+    public void CubeSpawn()
+    {
+        NewObject();
+        cube.SetActive(true);
+        sphere.SetActive(false);
+        capsule.SetActive(false);
+    }
+
+    public void SphereSpwan()
+    {
+        NewObject();
+        cube.SetActive(false);
+        sphere.SetActive(true);
+        capsule.SetActive(false);
+    }
+
+    public void CapsuleSpawn()
+    {
+        NewObject();
+        capsule.SetActive(true);
+        cube.SetActive(false);
+        sphere.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Objects/Cube.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cube : MonoBehaviour {
+
+    public float size = 1;
+    public float speed = 10;
+
+
+
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Rotate(0, speed * Time.deltaTime, 0);
+
+        transform.localScale = new Vector3(size, size, size);
+	}
+
+    public void AdjustSpeed(float newSpeed)
+    {
+        speed = newSpeed;
+    }
+
+    public void AdjustSize(float newSize)
+    {
+        size = newSize;
+    }
+}
